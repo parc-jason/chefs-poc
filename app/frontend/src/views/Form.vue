98f9905134a307c503c05b064cb671c74128ea99
Jeremy Ho <jujaga@gmail.com>
2020-07-20 15:59:55 -0700

    Mount a non-styled "uncontrolled embed" form
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/views/Form.vue
+++ app/frontend/src/views/Form.vue
@@ -1,11 +1,25 @@
 <template>
   <v-container>
     <h1 class="my-6 text-center">Form</h1>
+    <formio
+      src="https://chefs-jujaga-wxpbtr-dev.pathfinder.gov.bc.ca/bcgov-common/bcgovcommonfeedback"
+      v-on:submit="onSubmitMethod"
+    />
   </v-container>
 </template>
 
 <script>
+import { Form } from 'vue-formio';
+
 export default {
-  name: 'Form'
+  name: 'Form',
+  components: {
+    formio: Form
+  },
+  methods: {
+    onSubmitMethod(submission) {
+      console.log(submission); // eslint-disable-line no-console
+    }
+  }
 };
 </script>

f22053f79b7bf5d56a3d2dcd12f9f89a23b73ae8
Jeremy Ho <jujaga@gmail.com>
2020-07-20 17:08:17 -0700

    Wrap formio into separate component Initial exploration on formio events and data bindings
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/src/components/FormEmbed.vue
@@ -0,0 +1,42 @@
+<template>
+  <Formio
+    :form="form"
+    :src="source"
+    :submission="submission"
+    v-on:change="onChangeMethod"
+    v-on:submit="onSubmitMethod"
+  />
+</template>
+
+<script>
+import { Form } from 'vue-formio';
+
+export default {
+  name: 'FormEmbed',
+  components: {
+    Formio: Form
+  },
+  data: () => ({
+    form: {},
+    source:
+      'https://chefs-jujaga-wxpbtr-dev.pathfinder.gov.bc.ca/bcgov-common/bcgovcommonfeedback',
+    submission: {
+      data: {
+        feedbackOnFormIoTool: 'test'
+      }
+    }
+  }),
+  methods: {
+    onChangeMethod(change) {
+      const changed = change.changed;
+      console.debug('change', changed); // eslint-disable-line no-console
+      if (changed) {
+        this.submission.data[changed.instance.path] = changed.value;
+      }
+    },
+    onSubmitMethod(submission) {
+      console.debug('submit', submission); // eslint-disable-line no-console
+    }
+  }
+};
+</script>
--- app/frontend/src/views/Form.vue
+++ app/frontend/src/views/Form.vue
@@ -1,25 +1,17 @@
 <template>
   <v-container>
     <h1 class="my-6 text-center">Form</h1>
-    <formio
-      src="https://chefs-jujaga-wxpbtr-dev.pathfinder.gov.bc.ca/bcgov-common/bcgovcommonfeedback"
-      v-on:submit="onSubmitMethod"
-    />
+    <FormEmbed />
   </v-container>
 </template>
 
 <script>
-import { Form } from 'vue-formio';
+import FormEmbed from '@/components/FormEmbed.vue';
 
 export default {
   name: 'Form',
   components: {
-    formio: Form
-  },
-  methods: {
-    onSubmitMethod(submission) {
-      console.log(submission); // eslint-disable-line no-console
-    }
+    FormEmbed
   }
 };
 </script>

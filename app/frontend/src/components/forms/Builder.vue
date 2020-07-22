7b294f309d93cbdf65ccf7140772cb774d7a85ed
Jeremy Ho <jujaga@gmail.com>
2020-07-21 17:05:14 -0700

    Add missing formbuilder css and js
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/components/forms/Builder.vue
+++ app/frontend/src/components/forms/Builder.vue
@@ -1,9 +1,16 @@
 <template>
-  <FormBuilder
-    :form="{ display: 'form' }"
-    :options="{}"
-    @change="onChangeMethod"
-  />
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="https://unpkg.com/formiojs@latest/dist/formio.full.min.css" />
+    <FormBuilder :form="{ display: 'form' }" :options="{}" @change="onChangeMethod" />
+  </div>
 </template>
 
 <script>
@@ -17,7 +24,7 @@ export default {
   methods: {
     onChangeMethod(schema) {
       console.debug('schema', schema); // eslint-disable-line no-console
-    },
+    }
   }
 };
 </script>
